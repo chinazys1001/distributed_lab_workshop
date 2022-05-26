770048392f00e81bb3b061cad86ed29ccc86f373
chinazys1001 <100724928+chinazys1001@users.noreply.github.com>
2022-05-26 23:13:46 +0300

    Task 1. Large numbers

---
+++ Task 1. Large numbers/LargeNumbers.py
@@ -0,0 +1,34 @@
+from random import randint
+from datetime import datetime
+
+# Part 1. Calculating & printing number of keys set as a bit sequence
+for length in range (3, 13):
+    # current length
+    print(2 ** length, end="-bit sequence: ")
+    print(2 ** (2 ** length))
+    print('\n')
+    
+# Testing lengths are 3, 4, 5, 6, 7, 8 for Part 2 & 3 
+for test_length in range (3, 9):
+    # Part 2. Generating & printing random hex value of given length
+    print('\n')
+    print("Random key of length ", end = "")
+    print(test_length, end = ": ")
+    # Random hex in range 0x10...0 -> 0xF...F
+    rand_suffix = hex(randint(16 ** (test_length - 1), 16 ** test_length - 1))
+    # Random symbol in range 0x0 -> 0xF
+    rand_prefix = hex(randint(0, 15))
+    # Merging to get key in range 0x0...0 -> 0xF...F
+    rand_hex_key = rand_prefix
+    for i in range (3, len(rand_suffix)):
+        rand_hex_key = rand_hex_key + rand_suffix[i]
+    print(rand_hex_key)
+
+    # Part 3. Brute forcing hex values to find a match to rand_hex_key 
+    start_time = datetime.now()
+    for runner in range (16 ** (test_length - 1), 16 ** test_length):
+        if hex(runner) == rand_hex_key:
+            print("Execution time: ", end = "")
+            print((datetime.now() - start_time).microseconds / 1000 + (datetime.now() - start_time).seconds * 1000, end = "ms")
+    if test_length == 8:
+        print('\n')

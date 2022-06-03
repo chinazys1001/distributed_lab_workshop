01983da44a5ba585d2ab4197761b84ad36858760
chinazys1001 <100724928+chinazys1001@users.noreply.github.com>
2022-06-03 14:15:57 +0300

    Task 2

--- Task 2/HEXtoINT.py
+++ Task 2/HEXtoINT.py
@@ -1,3 +1,37 @@
-import math
+# converting hex digit to decimal number 
+def hexToDec (hex_value):
+    ascii_value = ord(hex_value)
+    if ascii_value < 58: return ascii_value - 48
+    if ascii_value < 71: return ascii_value - 55
+    return ascii_value - 87
 
-input_hex = input()
+# converting 1 byte (<=> 2 hex) to decimal number
+def getByteDecValue(hex1, hex2):
+    value1 = hexToDec(hex1)
+    value2 = hexToDec(hex2)
+    return 16 * value1 + value2
+
+print("Value: ", end = "")
+# if value is too large for console input, paste it below manually instead of "input()" 
+hex_string = input() # "large string goes here"
+hex_string = hex_string[2:] # removing 0x identifier
+if (len(hex_string) % 2 > 0):
+    hex_string = '0' + hex_string # adding leading zero if needed
+number_of_bytes = int(len(hex_string) / 2)
+little_endian = 0    
+big_endian = 0
+
+print("Number of bytes: ", end = "")
+print(number_of_bytes)
+
+# processing each byte
+for i in range (0, number_of_bytes):
+    ltr_byte_dec_value = getByteDecValue(hex_string[2 * i], hex_string[2 * i + 1])
+    little_endian += ltr_byte_dec_value * pow(256, i)
+    rtl_byte_dec_value = getByteDecValue(hex_string[2 * number_of_bytes - 2 * i - 2], hex_string[2 * number_of_bytes - 2 * i - 1])
+    big_endian += rtl_byte_dec_value * pow(256, i)
+
+print("Little-endian: ", end = "")
+print(little_endian)
+print("Big-endian: ", end = "")
+print(big_endian)
---
+++ Task 2/INTtoHEX.py
@@ -0,0 +1,30 @@
+# converting decimal remainer to hex digit 
+def decToHex (dec_value):
+    if (dec_value > 9): return chr(55 + dec_value)
+    return  chr(48 + dec_value)
+
+# converting big-endien to little-endien
+def getReversedBytes (hex_value):
+    reversed_bytes = ""
+    number_of_bytes = int(len(hex_value) / 2)
+    for i in range(0, number_of_bytes):
+        reversed_bytes += hex_value[2 * (number_of_bytes - i) - 2] + hex_value[2 * (number_of_bytes - i) - 1]
+    return reversed_bytes
+
+print("Value: ", end = "")
+dec_value = int(input()) # replace with large integer manually if needed
+hex_value = ""
+
+# converting input dec value to hex
+while dec_value > 0:
+    hex_value = decToHex(int(dec_value % 16)) + hex_value
+    dec_value = dec_value // 16
+
+# case the input value was written in little-endian order
+print("Hex value of little-endian value: ", end = "0x")
+if (len(hex_value) % 2 != 0): hex_value = '0' + hex_value # adding leading zero to pad last byte
+print(getReversedBytes(hex_value))
+
+# case the input value was written in big-endian order
+print("Hex value of big-endian value: ", end = "0x")
+print(hex_value)
